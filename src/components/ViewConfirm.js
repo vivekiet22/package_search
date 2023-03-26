2f75f36753ec526fbc519ab2fa0b3b661f3ab596
Fuji-P <nyaguri0417@gmail.com>
2023-03-27 01:33:12 +0900

    fix issue #7

--- src/components/ViewConfirm.js
+++ src/components/ViewConfirm.js
@@ -1,13 +1,42 @@
 import React from "react";
 
-const ViewConfirm = () => {
+const ViewConfirm = ({ value }) => {
   return (
     <div>
       <i
         className="btn fa fa-eye"
         style={{ color: "#555553", outline: "none" }}
+        data-toggle="modal"
+        data-target={`#viewModalCenter${value.name}`}
       ></i>
 
+      <div
+        className="modal fade"
+        id={`viewModalCenter${value.name}`}
+        tabIndex="-1"
+        role="dialog"
+        data-backdrop="false"
+        aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLongTitle">{value.name}</h5>
+            </div>
+            <div className="modal-body">{value.reason}</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-primary"
+                data-dismiss="modal"
+              >
+                OK
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
